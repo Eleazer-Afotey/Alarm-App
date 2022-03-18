173b475944b12a45134c93cf866fb47a41d27ff6
Eleazer Afotey <eleazerallnice@gmail.com>
2022-03-18 11:43:22 -0400

    added setDetails

--- app/src/main/java/com/example/alarm_app/MainActivity.java
+++ app/src/main/java/com/example/alarm_app/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.alarm_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button setAlarm = findViewById(R.id.button);
+    Button removeAlarm = findViewById(R.id.button2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent in = new Intent(MainActivity.this, SetDetails.class);
+                startActivity(in);
+
+            }
+        });
+
+        removeAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this, SetDetails.class);
+                startActivity(in);
+            }
+        });
+
+
+
+
+
     }
 }
